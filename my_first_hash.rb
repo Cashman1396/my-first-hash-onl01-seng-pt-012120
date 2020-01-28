f2473d5fc681f18a326666d8cad21c0b8ab1930e
Chris Ashman <chrisashman96@gmail.com>
2020-01-28 22:36:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,9 @@ end
 
 
 def shipping_manifest
-  the_manifest = { "whale bone corsets" => 5
+  the_manifest = { "whale bone corsets" => 5, 
+  "porcelain vases" => 2,
+  "oil paintings" = > 3
   }
 
 
@@ -17,8 +19,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -27,14 +28,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  
+  shipping_manifest["muskets"] = 2,
+  
+  shipping_manifest["gun powder"] = 4
+  
+  shipping_manifest
 end
